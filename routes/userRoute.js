2d066f6bf054e72a7ae74ef467b77ce07ef16b4c
3bd2lr7man <prog.3bd2lr7man@gmail.com>
2024-03-23 03:12:04 +0200

    change profile pic added

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const { protect } = require("../services/authService");
-const { getAllUsers, changeAvatar } = require("../services/userService");
+const { getAllUsers, changeProfilePic } = require("../services/userService");
 router.get("/", protect, getAllUsers);
-router.post("/changeuseravatar", protect, changeAvatar);
+// router.post("/changeuseravatar", protect, changeAvatar);
+router.post("/changeProfilePic", protect, changeProfilePic);
 module.exports = router;
--- services/userService.js
+++ services/userService.js
@@ -13,18 +13,32 @@ const getAllUsers = asyncHandler(async (req, res, next) => {
   });
 });
 
-const changeAvatar = asyncHandler(async (req, res, next) => {
+// const changeAvatar = asyncHandler(async (req, res, next) => {
+//   const userId = req.user._id;
+//   const user = await User.findById(userId);
+//   const Random6D = Math.floor(100000 + Math.random() * 900000).toString();
+//   const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${Random6D}`;
+//   const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${Random6D}`;
+//   user.profilePic = user.gender === "male" ? boyProfilePic : girlProfilePic;
+//   await user.save();
+//   res.status(200).json({
+//     statuss: "success",
+//     message: "user avatar changed successfully ",
+//     newAvatar: user.profilePic,
+//   });
+// });
+
+const changeProfilePic = asyncHandler(async (req, res, next) => {
   const userId = req.user._id;
   const user = await User.findById(userId);
-  const Random6D = Math.floor(100000 + Math.random() * 900000).toString();
-  const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${Random6D}`;
-  const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${Random6D}`;
-  user.profilePic = user.gender === "male" ? boyProfilePic : girlProfilePic;
+  const newProfilePic = req.body.newProfilePic;
+  user.profilePic = newProfilePic;
   await user.save();
   res.status(200).json({
-    statuss: "success",
-    message: "user avatar changed successfully ",
-    newAvatar: user.profilePic,
+    status: "success",
+    message: "user profile picture changed successfully ",
+    newProfilePic: user.profilePic,
   });
 });
-module.exports = { getAllUsers, changeAvatar };
+
+module.exports = { getAllUsers, changeProfilePic };
